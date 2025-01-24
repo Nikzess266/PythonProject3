b12c593d3faebd19bea97890f5a49b64fe215d7d
User <ukrtop95@gmail.com>
2025-01-24 11:28:37 +0200

    Initial commit

---
+++ 3.py
@@ -0,0 +1,9 @@
+import math
+from math import *
+
+a=float(input("Введіть ширину річки в метрах: "))
+b=float(input("Введіть відстань над водою в метрах: "))
+c=math.sqrt(a**2 + b**2)
+
+
+print("Висота тополі становить:", c, "м")
